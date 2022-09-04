9e92e3bd496226c18f1fe46b0a57f8e3b5caf5ed
danilojezernik <dani.jezernik@gmail.com>
2022-09-04 12:14:53 +0200

    Routes changed and nested

--- routes/users.js
+++ routes/users.js
@@ -2,40 +2,33 @@ const express = require('express');
 const db = require('../models');
 const router = express.Router();
 
-/* GET users */
-router.get('/', (req, res) => {
-    return db.Users.findAll().then((data) => res.json(data))
-});
 
-/* GET user */
-router.get('/:id', (req, res) => {
-    return db.Users.findOne(
-        {
-            where: {id: req.params.id}
-        }).then((data) => res.json(data));
-})
+router.route('/')
+    /* GET users */
+    .get((req, res) => {
+        db.Users.findAll().then((data) => res.json(data))
+    })
+    /* CREATE user */
+    .post((req, res) => {
+        db.Users.create(req.body)
+            .then((data) => res.json(data))
+    })
 
-/* CREATE user */
-router.post('/', (req, res) => {
-    return db.Users.create(req.body)
-        .then((data) => res.json(data))
-})
-
-/* UPDATE user */
-router.post('/:id/update', (req, res) => {
-    return db.Users.create(req.body,
-        {
-            where: {id: req.params.id}
-        }).then((data) => res.json(data))
-})
-
-/* DELETE user */
-router.delete('/:id', (req, res) => {
-    return db.Users.destroy(
-        {
-            where:
-                {id: req.params.id}
-        }).then((data) => res.json(data))
-})
+router.route('/:id')
+    /* GET user */
+    .get((req, res) => {
+        db.Users.findOne({where: {id: req.params.id}})
+            .then((data) => res.json(data));
+    })
+    /* UPDATE user */
+    .post((req, res) => {
+        db.Users.create(req.body, { where: {id: req.params.id}})
+            .then((data) => res.json(data))
+    })
+    /* DELETE user */
+    .delete((req, res) => {
+        db.Users.destroy({ where: {id: req.params.id} })
+            .then((data) => res.json(data))
+    })
 
 module.exports = router;
